48f2ae971557bb56ea78ac8266f004140c5a1418
NguyenDucguyen <nguyenhaibatrung05@gmail.com>
2024-12-21 19:45:12 +0700

    update pom , config and properties

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -3,12 +3,13 @@ package vn.hoidanit.laptopshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-//@SpringBootApplication
 
-@SpringBootApplication(exclude = {
-	org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-	org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
-})
+
+//@SpringBootApplication(exclude = {
+//	org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+//	org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+//})
+@SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
@@ -22,3 +23,5 @@ public class LaptopshopApplication {
 	}
 
 }
+
+
--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,35 +1,46 @@
 package vn.hoidanit.laptopshop.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+//import org.springframework.web.bind.annotation.RestController;
 import vn.hoidanit.laptopshop.service.UserService;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
 
 
-//@Controller
+
+
+@Controller
+public class UserController{
+  private UserService userService;
+  public UserController(UserService userService) {
+      this.userService = userService;
+   }
+
+  @RequestMapping("/hoidanit") 
+  public String getHomePage(){
+    String test = this.userService.handleHello();
+      return "hello";
+ }
+
+
+ 
+}
+
+//@RestController
 //public class UserController{
 
-//  @RequestMapping("/") 
-//   public String getHomePage(){
-//      return "hello from controller";
+//    private UserService userService;
+    
+ //   public UserController(UserService userService) {
+ //       this.userService = userService;
+ //   }
+
+ //   @GetMapping("")
+ //   public String getHomePage(){
+ //     return this.userService.handleHello();
  //}
 //}
-
-@RestController
-public class UserController{
-
-    private UserService userService;
-    
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @GetMapping("")
-    public String getHomePage(){
-      return this.userService.handleHello();
- }
-}
