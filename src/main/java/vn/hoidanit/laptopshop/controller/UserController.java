8f6f8ec5d54ddace4f816eaef6f16e385f7d4e1e
NguyenDucguyen <nguyenhaibatrung05@gmail.com>
2024-12-24 23:36:28 +0700

    create table_user.jsp , fix create.jsp, update controller

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,27 +1,20 @@
 package vn.hoidanit.laptopshop.controller;
 
 
-//import org.springframework.web.bind.annotation.RestController;
-import vn.hoidanit.laptopshop.service.UserService;
-
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.ui.Model;
 
 import vn.hoidanit.laptopshop.domain.User;
-
-
-
-
-
-
+//import org.springframework.web.bind.annotation.RestController;
+import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
 public class UserController{
@@ -44,34 +37,27 @@ public class UserController{
 
   @RequestMapping("/admin/user")
   public String getUserPage(Model model) {
+    List<User> users = this.userService.getAllUsers();
+    model.addAttribute("users1", users);
+    return "admin/user/table-user";
+  }
+
+   @RequestMapping( "/admin/user/create")
+  public String getCreateUserPage(Model model) {
     model.addAttribute("newUser",new User());
-    model.addAttribute("hoidanit","from controller with model");
     return "admin/user/create";
   }
 
-  @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+  @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
   public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
-    System.out.println("run here " + hoidanit);
     this.userService.handleSaveUser(hoidanit);
-    return "hello";
-  }
+    return "redirect:/admin/user";
   
 
+  }
 
- 
+
+  
+  
 }
 
-//@RestController
-//public class UserController{
-
-//    private UserService userService;
-    
- //   public UserController(UserService userService) {
- //       this.userService = userService;
- //   }
-
- //   @GetMapping("")
- //   public String getHomePage(){
- //     return this.userService.handleHello();
- //}
-//}
