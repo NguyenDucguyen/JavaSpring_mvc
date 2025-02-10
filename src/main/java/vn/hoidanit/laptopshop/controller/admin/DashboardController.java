944e22ff6b367b43114a6b25798bbd2a1efb01ae
NguyenDucguyen <nguyenhaibatrung05@gmail.com>
2025-02-10 17:15:15 +0700

    fix properties and update upload file on create.createjsp

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/DashboardController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/DashboardController.java
@@ -2,16 +2,16 @@ package vn.hoidanit.laptopshop.controller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
-@Controller
+
+
+
+@Controller 
 public class DashboardController {
-
-     @GetMapping("/admin")    
-     public String getDashboard(){
+    
+    @GetMapping("/admin")
+    public String getDashboard(){
         return "admin/dashboard/show";
-     }
-
-     
-}
+    }
+}    
