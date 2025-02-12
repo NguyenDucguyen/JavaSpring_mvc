f38d991264a8cd83974c17ff0f8f54217ffaf74e
NguyenDucguyen <nguyenhaibatrung05@gmail.com>
2025-02-12 23:03:29 +0700

    create hashPassword

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
@@ -7,6 +7,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -23,11 +25,13 @@ public class UserController {
 
     private final UserService userService;
     private final UploadService uploadService;
+    private final PasswordEncoder passwordEncoder;
          
         
-        public UserController(UserService userService,UploadService uploadService ) {
+        public UserController(UserService userService,UploadService uploadService,PasswordEncoder passwordEncoder ) {
                 this.userService = userService;
                 this.uploadService = uploadService;
+                this.passwordEncoder = passwordEncoder;
         }
 
     @RequestMapping("/")
@@ -66,8 +70,12 @@ public class UserController {
             @ModelAttribute("newUser") User hoidanit,
             @RequestParam("hoidanitFile") MultipartFile file) {
             
-            String  avatar = this.uploadService.handleSaveUploadFile(file,"avatar");
-        // this.userService.handleSaveUser(hoidanit);
+        String  avatar = this.uploadService.handleSaveUploadFile(file,"avatar");
+        String hashPassword = this.passwordEncoder.encode(hoidanit.getPassword());
+       
+        hoidanit.setAvatar(avatar);
+        hoidanit.setPassword(hashPassword);
+        this.userService.handleSaveUser(hoidanit);
         return "redirect:/admin/user";
     }
 
